ae7438a7282b204efb6f701a20989ff570d2ec6b
DryanLam <dryanlam@gmail.com>
2020-10-16 01:03:59 +0700

    MongoDb

--- Docker/backup.sh
+++ Docker/backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker exec mysql-db /usr/bin/mysqldump -uroot -pdryan tadp > db_dump.sql
+docker exec -i mongo-db mongodump --archive --gzip --db tadp > mongo_dump.gz
+docker exec mysql-db /usr/bin/mysqldump -uroot -pdryan tadp > mysql_dump.sql
