97f9f6e368b3159b517c45ef0fb6ddfb3e420f87
DryanLam <dryanlam@gmail.com>
2020-10-16 02:13:51 +0700

    Redis brief

--- Brief/python_brief.py
+++ Brief/python_brief.py
@@ -98,4 +98,13 @@ def index():
 if __name__ == '__main__':
     app.run()
     
-# -----------------------------
+# ----------------------------- REDIS
+import redis
+
+# r = redis.Redis("localhost", port=6379)
+r = redis.Redis("localhost")        # Default listen all port 6379
+v = r.get("name")
+print(v)
+
+print(r.set("tadp","microservices"))
+print(r.set("count",1))
