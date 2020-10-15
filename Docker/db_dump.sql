b50e6d08505004a1b3188d74ae3e90d553c99f64
DryanLam <dryanlam@gmail.com>
2020-10-15 11:37:38 +0700

    MySQL - tbl_users

--- Docker/db_dump.sql
+++ Docker/db_dump.sql
@@ -16,56 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `MyGuests`
+-- Table structure for table `tbl_users`
 --
 
-DROP TABLE IF EXISTS `MyGuests`;
+DROP TABLE IF EXISTS `tbl_users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `MyGuests` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(30) NOT NULL,
-  `lastname` varchar(30) NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+CREATE TABLE `tbl_users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `MyGuests`
---
-
-LOCK TABLES `MyGuests` WRITE;
-/*!40000 ALTER TABLE `MyGuests` DISABLE KEYS */;
-INSERT INTO `MyGuests` VALUES (1,'John','Doe','john@example.com','2020-10-14 21:46:14');
-/*!40000 ALTER TABLE `MyGuests` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `projects`
---
-
-DROP TABLE IF EXISTS `projects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `projects` (
-  `project_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
-  PRIMARY KEY (`project_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `projects`
+-- Dumping data for table `tbl_users`
 --
 
-LOCK TABLES `projects` WRITE;
-/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,'AI for Marketing','2019-08-01','2019-12-31'),(2,'ML for Sales','2019-05-15','2019-11-20');
-/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+LOCK TABLES `tbl_users` WRITE;
+/*!40000 ALTER TABLE `tbl_users` DISABLE KEYS */;
+INSERT INTO `tbl_users` VALUES (1,'dunglam@gmail.com','pbkdf2:sha256:150000$wEtLemFP$9dffba29df3c909ce72a73587ed04cdc94449646fe2474c877dea81f18a9510d','dunglam',0,'2020-10-15 04:21:11','2020-10-15 04:21:11'),(2,'dryan@gmail.com','pbkdf2:sha256:150000$QqC51c6l$7530e0f608f143da29fa58d2f5b3739571556e163b1db5797ba5844e99852971','dryan',0,'2020-10-15 04:26:14','2020-10-15 04:26:14');
+/*!40000 ALTER TABLE `tbl_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -77,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-14 22:11:44
+-- Dump completed on 2020-10-15  4:36:17
