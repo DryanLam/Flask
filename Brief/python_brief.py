52dfe68189808719751356b65df8a567496c78be
DryanLam <dryanlam@gmail.com>
2020-10-15 18:58:43 +0700

    Noted down

--- Brief/python_brief.py
+++ Brief/python_brief.py
@@ -50,3 +50,52 @@ with engine.connect() as con:
 # -----------------------------
 # Database manager - Idea for multi-connector
 https://medium.com/opex-analytics/database-connections-in-python-extensible-reusable-and-secure-56ebcf9c67fe
+
+
+
+# -----------------------------
+# Variable from Jinja2 form
+<HTML>
+<BODY bgcolor="cyan">
+<form method="POST" action="">
+    <center>
+    <H1>Enter your details </H1> <br>
+    First Name <input type = "text" name= "fname" /> <br>
+    Last Name <input type = "text" name = "lname" /> <br>
+    <input type = "submit">
+    </center>
+</form>
+</BODY>
+</HTML>
+
+#---
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'MyDB'
+
+mysql = MySQL(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+      if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+      return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+    
+# -----------------------------
