5a197638b1162aacc3b15d2e6e10f11b5d7a4beb
DryanLam <dryanlam@gmail.com>
2020-10-16 17:27:29 +0700

    Order ui

--- Prepare/views/index/__init__.py
+++ Prepare/views/index/__init__.py
@@ -11,3 +11,22 @@ def index():
         return render_template('index.html')
     else:
         return redirect(url_for('login_blp.getLogin'))
+
+
+@index_blp.route('/', methods = ['POST'])
+def doOrder():
+    _user = session['email']
+    _price = 999
+    _qty = int(request.form["quantity"])
+    _cost = _qty  * _price
+    return redirect(url_for('index_blp.getOrder', user=_user, qty=_qty, price=_price, cost=_cost))
+    # return render_template('order.html', user=_user, qty=_qty, price=_price, cost=_cost)
+
+
+@index_blp.route('/order', methods = ['GET'])
+def getOrder():
+    _user=request.args.get('user')
+    _qty=request.args.get('qty')
+    _price=request.args.get('price')
+    _cost=request.args.get('cost')
+    return render_template('order.html',user=_user, qty=_qty, price=_price, cost=_cost)
