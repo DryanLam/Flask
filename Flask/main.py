8cb4f8e36f97c70389cec71e33b4f245d642fa1c
DryanLam <dryanlam@gmail.com>
2020-10-17 09:17:21 +0700

    Restructure code

--- Flask/main.py
+++
@@ -1,7 +0,0 @@
-from products import app
-
-if __name__ == '__main__':
-	app.config.from_object('config.ConfigDev')
-	app.run(debug=True)
-
-
--- Flask/runner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export FLASK_APP=main.py
-
-flask run -p 3500 --reload --debugger
