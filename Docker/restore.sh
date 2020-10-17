8b45395a89232f4a9d0c2b637e613494bd2c87c5
DryanLam <dryanlam@gmail.com>
2020-10-17 23:54:24 +0700

    WIP: provisioning

--- Docker/restore.sh
+++ Docker/restore.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+chmod 777 -R redis_dump
+docker-compose up -d
+
 docker exec -i mongo-db mongorestore --archive --gzip < mongo_dump.gz
 docker exec -i mysql-db /usr/bin/mysql -uroot -pdryan --force tadp < mysql_dump.sql
