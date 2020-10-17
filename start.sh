07e2bc8dd642b6ad2f90f50571df28ae0e8c2068
DryanLam <dryanlam@gmail.com>
2020-10-17 12:29:23 +0700

    RC 2.0.0

--- start.sh
+++ start.sh
@@ -6,5 +6,13 @@ docker-compose -f Docker/docker-compose.yml up -d
 
 sh Docker/restore.sh
 
-sh API/env_dev.sh
-sh API/runner.sh
+# sh API/env_dev.sh
+# sh API/runner.sh
+
+
+docker run -i --name api python3:latest -v API:/home/test
+docker exec -d api pipenv install Pipfile & pipenv shell & sh runner.sh
+
+
+docker run -i --name webui python3:latest -v API:/home/test
+docker exec -d webui pipenv install Pipfile & pipenv shell & sh runner.sh
