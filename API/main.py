5f374f1146eac27312a2cdc42dbcca90bf6dbc46
DryanLam <dryanlam@gmail.com>
2020-10-19 11:40:11 +0700

    Dockerfile for API well

--- API/main.py
+++ API/main.py
@@ -1,8 +1,11 @@
 from views import app
+import os
 
+# app = Flask(__name__)
+port = int(os.environ.get("PORT", 3500))
 
 if __name__ == "__main__":
     app.config.from_object('config.DevelopmentConfig')
-    app.run(debug=True)
-    # app.run(port=3000)
+    app.run(debug=True,host='0.0.0.0',port=port)
+    # app.run(debug=True, port=port)
     
